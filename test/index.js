006e0dd522585bfc2710c213b875b2ed16f541c8
Michael <mvetkin@ozon.ru>
2025-02-17 17:47:54 +0300

    Added deep link ignore

--- test/index.js
+++ test/index.js
@@ -2225,11 +2225,25 @@ test('code (text)', async function (t) {
 })
 
 test('link', { only: true }, async function (t) {
-  await t.test('should support a relevant link', async function () {
-    t.runOnly(true);
+  await t.test('should support a relevant https link', async function () {
     assert.deepEqual(
       to({type: 'link', url: 'https://vk.com/post/la2sdf3lhl32', children: [{type: 'text', value: 'link'}]}),
       { text: 'link (https://vk.com/post/la2sdf3lhl32)', html: '<a class="text-entity-link" href="https://vk.com/post/la2sdf3lhl32" data-entity-type="MessageEntityTextUrl" dir="auto">link</a>' }
+    )
+  })
+
+  await t.test('should support a relevant http link', async function () {
+    assert.deepEqual(
+      to({type: 'link', url: 'http://vk.com/post/la2sdf3lhl32', children: [{type: 'text', value: 'link'}]}),
+      { text: 'link (http://vk.com/post/la2sdf3lhl32)', html: '<a class="text-entity-link" href="http://vk.com/post/la2sdf3lhl32" data-entity-type="MessageEntityTextUrl" dir="auto">link</a>' }
+    )
+  })
+
+  await t.test('should not support app deep link', async function () {
+    t.runOnly(true);
+    assert.deepEqual(
+      to({type: 'link', url: 'obsidian://show-plugin?id=tengwar', children: [{type: 'text', value: 'link'}]}),
+      { text: 'link', html: 'link' }
     )
   })
 
